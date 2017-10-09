aea9814c60e86a8b208c90956ca3428ab726330d
Joe Beltrami <jpbeltrami@gmail.com>
2017-10-09 14:32:28 -0700

    timer is set

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,8 @@
 // stories
 $(document).ready(function(){
-  // have to convert time into minutes
-  // convert minutes number into string
-  var minutes = Math.floor(1500 / 60);
-  var seconds = 0;
+  var second = 1000;
+  var minute = 60000;
+
   // adds extra 0
   function add0(number){
     if (number < 10){
@@ -13,7 +12,50 @@ $(document).ready(function(){
       return number.toString();
     }
   }
-  // what to do when
 
-  $("#timer").text(add0(minutes) + ':' + add0(seconds));
+  // endtime should be now + 25 minutes by default. default can be changed from a button
+  var default_work_time = new Date(Date.parse(new Date()) + (minute * 25));
+  var default_break_time = new Date(Date.parse(new Date()) + (minute * 5));
+
+  // calculate time remaining
+  function getTimeRemaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+      };
+  }
+
+  // countdown and stop at end time
+  // each second go back and re-render the clock
+  function initializeClock(endtime){
+    var timeinterval = setInterval(function(){
+      var t = getTimeRemaining(endtime);
+      if (t.total <= 0){
+        clearInterval(timeinterval);
+      }
+      $("#timer").text(add0(t.minutes) + ':' + add0(t.seconds));
+    },1000);
+  }
+
+  // when start is click initialize the countdown
+  $("#start").click(function() {
+    initializeClock(default_work_time);
+  });
+
+
+  // function addMinute(current_time){
+  //   return current_time + minute;
+  // }
+  //
+  // function subMinute(current_time){
+  //   return current_time - minute;
+  // }
 });
