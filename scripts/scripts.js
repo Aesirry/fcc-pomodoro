452d2bb24e827a744ec426a793c641ebc9eae494
Joe Beltrami <jpbeltrami@gmail.com>
2017-10-09 13:16:23 -0700

    adds function that adds 0 for single digit numbers

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,19 @@
 // stories
-// I can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.
-// I can reset the clock for my next pomodoro.
-// I can customize the length of each pomodoro.
+$(document).ready(function(){
+  // have to convert time into minutes
+  // convert minutes number into string
+  var minutes = Math.floor(1500 / 60);
+  var seconds = 0;
+  // adds extra 0
+  function add0(number){
+    if (number < 10){
+      return '0' + number.toString();
+    }
+    else {
+      return number.toString();
+    }
+  }
+  // what to do when
+
+  $("#timer").text(add0(minutes) + ':' + add0(seconds));
+});
