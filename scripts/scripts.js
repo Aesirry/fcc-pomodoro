9a089d2cd6b97a7972df90c169330115550c5ed9
Joe Beltrami <jpbeltrami@gmail.com>
2017-11-10 17:12:13 +0800

    start and reset buttons work

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,9 @@
 $(document).ready(function(){
   var second = 1000;
   var minute = 60000;
-  $("#timer").text('25:00');
-  // adds extra 0
+  var toggle = false;
+
+  // adds extra 0 to single digit numbers
   function add0(number){
     if (number < 10){
       return '0' + number.toString();
@@ -13,9 +14,24 @@ $(document).ready(function(){
     }
   }
 
-  // endtime should be now + 25 minutes by default. default can be changed from a button
-  var default_work_time = new Date(Date.parse(new Date()) + (minute * 25));
-  var default_break_time = new Date(Date.parse(new Date()) + (minute * 5));
+  // returns the current time of the work session
+  function work_time(custom_time){
+    if (custom_time == null){
+      return new Date(Date.parse(new Date()) + (minute * 25));
+    }
+    else {
+      return new Date(Date.parse(new Date()) + (minute * custom_time));
+    }
+  }
+
+  function break_time(custom_time){
+    if (custom_time == null){
+      return new Date(Date.parse(new Date()) + (minute * 5));
+    }
+    else {
+      return new Date(Date.parse(new Date()) + (minute * custom_time));
+    }
+  }
 
   // calculate time remaining
   function getTimeRemaining(endtime){
@@ -33,29 +49,46 @@ $(document).ready(function(){
       };
   }
 
-  // countdown and stop at end time
+  // start countdown and stop at end time
   // each second go back and re-render the clock
   function initializeClock(endtime){
+    $("#start").attr('id', 'stop');
+    $("#start").text("Stop");
     var timeinterval = setInterval(function(){
+
       var t = getTimeRemaining(endtime);
       if (t.total <= 0){
         clearInterval(timeinterval);
+        ("#timer").text("Time's Up!");
       }
+      $("#reset").click(function(){
+        reset(timeinterval);
+      })
       $("#timer").text(add0(t.minutes) + ':' + add0(t.seconds));
     },1000);
   }
 
-  // when start is click initialize the countdown
+  // saves current time when timer is stopped
+
+  // when start is clicked initialize the countdown
   $("#start").click(function() {
-    initializeClock(default_work_time);
+    initializeClock(work_time());
   });
 
+  // resets the clock to 25 minutes
+  function reset(current_interval){
+    clearInterval(current_interval);
+    $("#timer").text("25:00");
+  }
 
-  // function addMinute(current_time){
-  //   return current_time + minute;
-  // }
+  // stops and saves the current timer
+  function pause(){
+    // make initializeClock pass the current time to the stop function
+    // initializeClock
+    // stop function
+  }
+  // function add_1(){
+  //   reset(timeinterval);
   //
-  // function subMinute(current_time){
-  //   return current_time - minute;
   // }
 });
